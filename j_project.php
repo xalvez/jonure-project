0145f08ab1734c8df58aa478b22df14e5e86ea7c
xalvez <85679715+xalvez@users.noreply.github.com>
2023-12-23 18:27:01 +0300

    Add files via upload

---
+++ j_project.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta http-equiv="CONTENT-TYPE" content="text/html; charset=UTF-8">
+  <title>Hello, World!</title>
+  <link rel="stylesheet" href="j_project.css">
+</head>
+<body>
+  
+  <headr class="header-main">
+    
+    <nav>
+      <a href="j_project.php" class="logo">
+        <img src="pic1.jpg" alt="website logo">
+    </a>
+      <ul>
+        <li><a href="#">about us</a>
+        <ul>
+          <li><a href="#">who we are</a></li>
+          <li><a href="#">charity goals</a></li>
+        </ul>
+      </li>
+        <li><a href="#">contact</a></li>
+        <li><a href="#">how to help</a></li>
+        <li><a href="#">our events</a></li>
+        <li><a href="#">register</a></li>        
+      </ul>
+      
+    </nav>
+    <div class="sm">
+      <form action="" method="post">
+         <input type="text" name="search" placeholder="search.....">
+         <button type="submit"><img src="search_icon.png" alt="search button"></button>   
+      </form>
+  
+    </div>
+    
+  </headr>
+  
+  
+</body>
+</html>
